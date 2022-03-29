d460bf34958395c66dd4d4d18bb21746682f6ff4
Una LEstrange <lestrange.una@itsligo.ie>
2022-03-29 20:57:58 +0100

    changes for deployment to heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const app = express()
-const port =  proc.env.PORT || 3000
+const port =  process.env.PORT || 3000
 
 const cookieParser = require('cookie-parser');
 
